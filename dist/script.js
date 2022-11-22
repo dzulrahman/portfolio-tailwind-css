6a8e9e2170d82411dc79abf2179ba6b0b18c6fc3
zulrahman04 <55014705+zulrahman04@users.noreply.github.com>
2022-11-22 19:42:27 +0700

    portfolio-tailwind-css

---
+++ dist/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('#hamburger')
+const navmenu   = document.querySelector('#navmenu')
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active')
+    navmenu.classList.toggle('hidden')
+})
+
+window.onscroll = function(){
+    const header = document.querySelector('header')
+    const fixnav = header.offsetTop
+
+    if (window.pageYOffset > fixnav) {
+        header.classList.add('navbar-fixed')
+    }else{
+        header.classList.remove('navbar-fixed')
+    }
+}
